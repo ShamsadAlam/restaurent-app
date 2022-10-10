ac2af9f139367cb87aa624d49a59703cc82ea968
Shamsad Alam <nobleperson7571@gmail.com>
2022-10-10 22:49:34 +0530

    restaurent-app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Components/Basic/style.css";
+import Restaurent from "./Components/Basic/Restaurent";
+
+const App = () => {
+  return (
+    <>
+      <Restaurent />
+    </>
+  );
+};
+export default App;
